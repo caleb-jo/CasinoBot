701278f848e2cb258cfacf06f9fd79120f6f2059
Caleb <calebnj1999@gmail.com>
2024-04-12 15:15:59 -0400

    Updated to include calls to game files and construct reply message to user

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 const {Client, IntentsBitField} = require('discord.js');
 const fs = require('fs');
 
+
+// GAMES
+const SLOTS = require('./games/slots');
+const ROULETTE = require('./games/roulette');
+
+
 let DiscordBotToken;
 let ReplyMessage;
 let IsHelpRequest;
+let UserMessage;
 
 
 
@@ -36,22 +43,66 @@ function ClientLogin() {
 }
 
 function ParseMessage(message) {
-    CoreMessage = message.content.slice(8, 100); //remove !gamble from beginning because I don't like it
-    console.log(`Message received: ${CoreMessage})`);
 
-    IsHelpRequest = CoreMessage.match(/--help/);
+    let Gambler, Game, Wager = 0; //init vars
+
+    CoreMessage = message.content.slice(8, 100); //remove !gamble from beginning because I don't like it
+    console.log(`Message received: ${CoreMessage}`);
+
+    IsHelpRequest = CoreMessage.match(/--[a-z]+/g);
     let Numbers = CoreMessage.match(/\d+/g);
     let Words = CoreMessage.match(/([a-z]+)/g);
-    console.log(Numbers)
-    console.log(Words)
+
+
+    Gambler = message.author.id;
+    if (Numbers){
+        Wager = Number(Numbers[0]); //wager is always first number
+    }
+
+    Game = SelectGame(Words, Numbers);
+
 
     if (IsHelpRequest) {
-        message.reply('Welcome to CasinoBot! I am here to serve all your gambling and ATM needs from the comfort of your discord server.\n\nList of Games:\nSlots: "!gamble slots {amount to wager} {number of lines (1-5)}"\nWager amount is multiplied by number of lines.\n\nRoulette: "!gamble roulette {amount to wager} {"red", "black", "1st", "2nd", "3rd"}"\nRed/black give 1:1 odds, first(1-12)/second(13-24)/third(25-36) give 1:2 odds\n\nNow we\'ve never seen anyone to have to do this, but feel free to take out a loan using the command: "!gamble loan {amount of money}"\n\nHappy Hunting!');
+        switch(IsHelpRequest[0]) {
+            case '--help':
+                ReplyMessage += 'Welcome to CasinoBot! I am here to serve all your gambling and ATM needs from the comfort of your discord server.\n\nList of Games:\nSlots: "!gamble slots {amount to wager} {number of lines (1-5)}"\n Wager amount is multiplied by number of lines.\n\nRoulette: "!gamble roulette {amount to wager} {"red", "black", "first", "second", "third"}"\nRed/black give 1:1 odds, first(1-12)/second(13-24)/third(25-36) give 1:2 odds\n\nHappy Hunting!';
+                message.reply(ReplyMessage);
+                break;
+        }
+        
     }
 
     return;
 }
 
+
+//ADD GAMES HERE AND 
+function SelectGame(Words, Numbers) {
+    if (Words) {
+        let FirstWord = Words[0];
+    
+    switch(FirstWord) {
+        case 'slots':
+
+            let Lines = Number(Numbers[1]);
+            if (Lines > 5 || Lines < 1 || !Lines) {
+                ReplyMessage = "Invalid number of lines. Please input a number between 1 and 5. Use --help for more information.";
+                UserMessage.reply(ReplyMessage);
+                return;
+            }
+            else {console.log(`Playing ${Lines} lines`)}
+            
+            SLOTS.play();
+            break;
+        case 'roulette':
+
+            
+            ROULETTE.play();
+            break;
+    
+        }
+    }
+}
 
 
 
@@ -63,15 +114,16 @@ client.on('ready', (cli) => {
 })
 
 client.on('messageCreate', (message) => {
-    ReplyMessage = '';
+    UserMessage = message;
+    
     if (message.author.bot) {
         return; //robots don't gamble
     }
 
     let BeginningOfMessage = message.content.slice(0,7);
     if (BeginningOfMessage == '!gamble') {
-        let Gambler, Game, Wager = 0;
-        Gambler, Game, Wager = ParseMessage(message);
+        ReplyMessage = '';
+        ParseMessage(message);
     }
     return
 });
