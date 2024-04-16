2f3b0dd9e17226d16acbd8759f8a248523fcbb71
Caleb <calebnj1999@gmail.com>
2024-04-16 02:26:00 -0400

    started and completed roulette game

--- src/games/roulette.js
+++ src/games/roulette.js
@@ -1,8 +1,39 @@
 // ROULETTE
 
+function GetRandomInt(min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
+
+function GetColor() {
+    Cell = GetRandomInt(1,37);
+    switch (true){
+        case (Cell <= 18): return 'red';
+        case (Cell <= 36): return 'black';
+        case (Cell === 37): return 'green';
+    }
+
+}
+
 module.exports = {
-    play: function(){
+    play: function(Wager, Color){
+        let IsAWinner = false;
+        let ResponseMessage = '';
+
+        let RolledColor = GetColor();
+        ResponseMessage += `:${RolledColor}_circle: :${RolledColor}_circle: :${RolledColor}_circle:`
         
+
+        if (Color === RolledColor){
+            IsAWinner = true;
+            BalanceAdjustment = Wager * 2;
+        }
+        else {
+            IsAWinner = false;
+            BalanceAdjustment = 0 - Wager;
+        }
+
+        return [IsAWinner, BalanceAdjustment, ResponseMessage];
+
     },
 }
 
