2de9602d7f320e433c83268aa39b17a397fb366c
Caleb <calebnj1999@gmail.com>
2024-04-16 02:24:19 -0400

    added --balance arg to check own balance, created reply function

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ let DiscordBotToken; // for data from TOKENFILE
 let ReplyMessage;  // global for constructed response message
 let IsHelpRequest; // move this to ParseMessage?
 let UserMessage; // global for message object
+let UserBalance; //global for user balance
 
 // client object has permissions (intents)
 const client = new Client({
@@ -56,9 +57,7 @@ function Balances(state, userid='', amount=0) {
 }
 */
 
-// interacts with USERBALANCEFILE to update user balances, adds new users to userbalance.json (gives 1,000,000)
-// update balance takes message.author.id as userid and amount gained/lost from bet
-function UpdateBalance(userid=0, amount=0) {
+function GetNewBalance(Gambler) {
     fs.readFile(USERBALANCEFILE, function (err, data) {
         if (err) {
             console.error(err);
@@ -71,14 +70,49 @@ function UpdateBalance(userid=0, amount=0) {
         JsonKeys = Object.keys(Json);
 
 
-        let CurrentBalance;
-        let NewBalance;
+        
+        
+        // in case of new user, initialize balance to 1,000,000
+        // else get value
+        if (!JsonKeys.includes(Gambler)) {
+            CurrentBalance = 1000000;       
+        }
+        else {
+            CurrentBalance = Json[Gambler];
+        }
+        UserBalance = CurrentBalance;
+        ReplyMessage = `Hey ${UserMessage.author}, you have ${UserBalance} in your bank account.`
+        UserMessage.reply(ReplyMessage);
+        UserBalance = 0; 
+    });
+
+}
+
+// interacts with USERBALANCEFILE to update user balances, adds new users to userbalance.json (gives 1,000,000)
+// update balance takes message.author.id as userid and amount gained/lost from bet
+function UpdateBalance(userid=0, amount=0) {
+    let CurrentBalance;
+    let NewBalance;
+
+    fs.readFile(USERBALANCEFILE, function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        
+        // parse data for json info and list keys (users in this case)
+        Json = JSON.parse(data);
+
+        JsonKeys = Object.keys(Json);
+
+
+        
         
         // in case of new user, initialize balance to 1,000,000
         // else get value
         if (!JsonKeys.includes(userid)) {
             console.log("a new addict >:)");
-            CurrentBalance = 1000000;            
+            CurrentBalance = 1000000;       
         }
         else {
             CurrentBalance = Json[userid];
@@ -88,6 +122,8 @@ function UpdateBalance(userid=0, amount=0) {
         NewBalance = CurrentBalance + amount;
 
         Json[userid] = NewBalance;
+
+        UserBalance = NewBalance;
         
         // console.log(JsonKeys);
 
@@ -96,14 +132,14 @@ function UpdateBalance(userid=0, amount=0) {
         
         // write updated json string back to file
         fs.writeFile(USERBALANCEFILE, Json, (err) => {
-            if (err) {6
+            if (err) {
                 console.error(err);
                 return;
             }
         });
-
-
     });
+
+    return;
 }
 
 // callback for GetToken, gives login token to client once it is read from file
@@ -112,11 +148,10 @@ function ClientLogin() {
     return
 }
 
-
 function ParseMessage(message) {
 
-    let Gambler, Game, Wager = 0;
-    // Gambler: discord user who sent the msg, Game: game played (slots, roulette, etc...), Wager: amount bet on game
+    let Gambler, Game;
+    // Game: game played (slots, roulette, etc...), Wager: amount bet on game
 
     CoreMessage = message.content.slice(8, 100); //remove !gamble from beginning of message
     // console.log(`Message received: ${CoreMessage}`);
@@ -128,19 +163,20 @@ function ParseMessage(message) {
 
 
     Gambler = message.author.id;
-    if (Numbers){
-        Wager = Number(Numbers[0]); //wager is always first number
-    }
+    
 
-    Game = SelectGame(Words, Numbers);
+    Game = SelectGame(Words, Numbers, Gambler);
 
 
     if (IsHelpRequest) {
         switch(IsHelpRequest[0]) {
             case '--help':
-                ReplyMessage += 'Welcome to CasinoBot! I am here to serve all your gambling and ATM needs from the comfort of your discord server.\n\nList of Games:\nSlots: "!gamble slots {amount to wager} {number of lines (1-5)}"\n Wager amount is multiplied by number of lines.\n\nRoulette: "!gamble roulette {amount to wager} {"red", "black", "first", "second", "third"}"\nRed/black give 1:1 odds, first(1-12)/second(13-24)/third(25-36) give 1:2 odds\n\nHappy Hunting!';
+                ReplyMessage = 'Welcome to CasinoBot! I am here to serve all your gambling and ATM needs from the comfort of your discord server. Use --balance to see your current balance!\n\nList of Games:\nSlots: "!gamble slots {amount to wager} {number of lines (1-5)}"\n Wager amount is multiplied by number of lines.\n\nRoulette: "!gamble roulette {amount to wager} {"red", "black"}\n\nHappy Hunting!';
                 message.reply(ReplyMessage);
                 break;
+            case '--balance':
+                GetNewBalance(Gambler);
+                // console.log('getting balance');
         } // need to exit this/provide catch all?
         
     }
@@ -148,15 +184,38 @@ function ParseMessage(message) {
     return;
 }
 
+function SendResponseMessage(WonGame, AmountDifference) {
+    AmountDifference = Math.abs(AmountDifference);
+
+    if (WonGame) {
+        ReplyMessage += `\n\nCongratulations! You won the game! You won $${AmountDifference}! Keep the streak alive!`
+        // console.log(ReplyMessage);
+    }
+    else {
+        ReplyMessage += `\n\nAww, tough luck hot shot. You can't win 'em all. Don't let this measly $${AmountDifference} loss keep you down!`
+        // console.log(ReplyMessage);
+    }
+
+    UserMessage.reply(ReplyMessage);
+}
 
 //ADD GAMES HERE AND TO /src/games FOLDER. REQUIRE AT TOP
-function SelectGame(Words, Numbers) {
+function SelectGame(Words, Numbers, Gambler) {
+    let HasWonTheGame = false;
+    let AmountDifference = 0;
+    let Wager;
+
+    if (Numbers){
+        Wager = Number(Numbers[0]); //wager is always first number
+    }
+
+
     if (Words) {
         let FirstWord = Words[0];
     
     switch(FirstWord) {
         case 'slots':
-        // template input message: !gamble slots {Wager} {Lines}
+        // template input message: !gamble slots {Wager (per line)} {Lines}
 
             let Lines = Number(Numbers[1]);
             if (Lines > 5 || Lines < 1 || !Lines) {
@@ -165,14 +224,26 @@ function SelectGame(Words, Numbers) {
                 return;
             }
             // else {console.log(`Playing ${Lines} lines`)}
+            [HasWonTheGame, AmountDifference, AddToMessage] = SLOTS.play(Wager, Lines);
             
-            SLOTS.play();
+
+            // use async/promises for this? removing "current balance" from SendResponseMessage for now...
+            UpdateBalance(Gambler, AmountDifference);
+
+            ReplyMessage += AddToMessage;
+
+            SendResponseMessage(HasWonTheGame, AmountDifference);
             break;
         case 'roulette':
-        // template input message !gamble roulette {Wager} {color(red/black}
+        // template input message !gamble roulette {Wager} {color(red/black)}
 
-            
-            ROULETTE.play();
+            Color = Words[1];
+
+            [HasWonTheGame, AmountDifference, AddToMessage] = ROULETTE.play(Wager, Color);
+
+            ReplyMessage += AddToMessage;
+
+            SendResponseMessage(HasWonTheGame, AmountDifference);
             break;
     
         }
