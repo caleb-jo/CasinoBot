0167c1b109a2e7751a68179cc7799589047e98ba
Caleb <calebnj1999@gmail.com>
2024-04-25 02:10:56 -0800

    Combined file read functions, centralized function calls to messageCreate

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ const USERBALANCEFILE = './token/json/userbalance.json';
 
 
 let DiscordBotToken; // for data from TOKENFILE
-let ReplyMessage;  // global for constructed response message
 let UserMessage; // global for message object
-let UserBalance; //global for user balance
+
+
 
 
 
@@ -33,30 +33,30 @@ function GetNewBalance(Gambler) {
 
         JsonKeys = Object.keys(Json);
 
-
-        
-        
         // in case of new user, initialize balance to 1,000,000
         // else get value
-        if (!JsonKeys.includes(Gambler)) {
-            CurrentBalance = 1000000;       
+        if (!JsonKeys.includes(Gambler.id)) {
+            CurrentBalance = 1000000;
         }
         else {
-            CurrentBalance = Json[Gambler];
+            CurrentBalance = Json[Gambler.id];
         }
         UserBalance = CurrentBalance;
-        ReplyMessage = `Hey ${UserMessage.author}, you have ${UserBalance} in your bank account.`
+        ReplyMessage = `Hey ${Gambler}, you have ${UserBalance} in your bank account.`
         UserMessage.reply(ReplyMessage);
         UserBalance = 0; 
     });
 
-}
+};
 
 // interacts with USERBALANCEFILE to update user balances, adds new users to userbalance.json (gives 1,000,000)
 // update balance takes message.author.id as userid and amount gained/lost from bet
-function UpdateBalance(userid=0, amount=0) {
+function UpdateBalance(user, amount=0) {
     let CurrentBalance;
     let NewBalance;
+    let User = user;
+    let UserId = user.id;
+    console.log(User, UserId);
 
     fs.readFile(USERBALANCEFILE, function (err, data) {
         if (err) {
@@ -64,63 +64,61 @@ function UpdateBalance(userid=0, amount=0) {
             return;
         }
         
-        // parse data for json info and list keys (users in this case)
         Json = JSON.parse(data);
 
         JsonKeys = Object.keys(Json);
 
-
-        
-        
-        // in case of new user, initialize balance to 1,000,000
-        // else get value
-        if (!JsonKeys.includes(userid)) {
-            console.log("a new addict >:)");
+        if (!JsonKeys.includes(user.id)) {
             CurrentBalance = 1000000;       
         }
         else {
-            CurrentBalance = Json[userid];
+            CurrentBalance = Json[user.id];
         }
 
-        // change value from file by amount, rebind that value to userid, stringify 
-        NewBalance = CurrentBalance + amount;
+        if(amount){
+            //since I don't think it's possible to bet and, win or lose, have amt=0
+            //use message.author.id to track amounts so changing username does not lose progress
+            NewBalance = CurrentBalance + amount;
 
-        Json[userid] = NewBalance;
+            Json[user.id] = NewBalance;
 
-        UserBalance = NewBalance;
-        
-        // console.log(JsonKeys);
+            UserBalance = NewBalance;
+            
+            // console.log(JsonKeys);
 
-        Json = JSON.stringify(Json);
-        
-        
-        // write updated json string back to file
-        fs.writeFile(USERBALANCEFILE, Json, (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        });
+            Json = JSON.stringify(Json);
+            
+            fs.writeFile(USERBALANCEFILE, Json, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            });
+        } else {
+            RespondWith(`Hey ${user}, you have ${CurrentBalance} in your bank account.`)
+        }
     });
 
     return;
-}
+};
 
 
-function ParseMessage(message) {
-// message = String
-// returns list of message phrases and cuts !gamble from front e.g. "!gamble roulette 500 black" --> ["roulette", 500, "black"], "!gamble --help roulette" --> ["--help", "roulette"]
 
-    CoreMessage = message.content.slice(8, 100); //remove !gamble from beginning of message
-    // console.log(`Message received: ${CoreMessage}`);
 
-    
-    // .match function returns list, regex more "resilient" than splitting on spaces
-    CoreMessage = CoreMessage.replace('—','--')
-    let Phrases = CoreMessage.match(/(--|)([a-zA-Z0-9]+)/g);
 
-    return Phrases;
-}
+
+//-----------------------RESPONSE_MESSAGES-------------------------------------
+
+function SendHelpMessage() {
+    ReplyMessage = 'Welcome to CasinoBot! I am here to serve all your gambling needs from the comfort of your discord server. Use --balance to see your current balance!\n\nList of Games:\nSlots: "!gamble slots {amount to wager} {number of lines (1-5)}"\n Wager amount is multiplied by number of lines.\n\nRoulette: "!gamble roulette {amount to wager} {"red", "black"}\n\nHappy Hunting!';
+    UserMessage.reply(ReplyMessage);
+    return;
+};
+
+function RespondWith(Message) {
+    UserMessage.reply(Message);
+    return;
+};
 
 function GenerateResultMessage(WonGame, AmountDifference) {
     AmountDifference = Math.abs(AmountDifference);
@@ -135,62 +133,66 @@ function GenerateResultMessage(WonGame, AmountDifference) {
     }
 
     return ReplyMessage;
-}
-
-function SendHelpMessage() {
-    ReplyMessage = 'Welcome to CasinoBot! I am here to serve all your gambling needs from the comfort of your discord server. Use --balance to see your current balance!\n\nList of Games:\nSlots: "!gamble slots {amount to wager} {number of lines (1-5)}"\n Wager amount is multiplied by number of lines.\n\nRoulette: "!gamble roulette {amount to wager} {"red", "black"}\n\nHappy Hunting!';
-    UserMessage.reply(ReplyMessage);
-    return;
-}
-
-
-function RespondWith(Message) {
-    UserMessage.reply(Message);
-    return;
 };
 
-// returns True if NO errors in message, add more things to this check as I think of/come across them
-function IsValidMessage(PhraseList) {
+
+
+//-----------------------MESSAGE_INPUT_PROCESSING-------------------------------------
+
+function ParseMessage(message) {
+// message = String
+// returns list of message phrases and cuts !gamble from front e.g. "!gamble roulette 500 black" --> ["roulette", 500, "black"], "!gamble --help roulette" --> ["--help", "roulette"]
+
+    CoreMessage = message.content.slice(8, 100); //remove !gamble from beginning of message
+    // console.log(`Message received: ${CoreMessage}`);
+
     
-    if(!PhraseList || !(typeof(PhraseList[0]) === "string")) {
-        SendHelpMessage();
-        return false;
+    // .match function returns list, regex more "resilient" than splitting on spaces
+    CoreMessage = CoreMessage.replace('—','--')
+    let Phrases = CoreMessage.match(/(--|)([a-zA-Z0-9]+)/g);
+
+    return Phrases;
+};
+
+function IsValidMessage(PhraseList) {
+    // returns True if NO errors in message, add more things to this check as I think of/come across them
+
+    switch(true) {
+        case (!PhraseList):
+        case (!PhraseList[0]):
+        case (typeof(PhraseList[0]) != "string"):
+            SendHelpMessage();
+            return false;
+
+        case (PhraseList[1] != PhraseList[1]):
+        case (PhraseList[1] <= 0):
+            RespondWith("Wager must be a positive number");
+            return false;
     }
 
     return true;
-}
+};
 
-
-//ADD GAMES HERE AND TO /src/games FOLDER AND REQUIRE ^^^
-function ExecuteGame(Phrases) {
+function ExecuteGame(Player, Phrases) {
+    //ADD GAMES HERE AND TO /src/games FOLDER AND REQUIRE ^^^
     let FirstWord = Phrases.shift();
     let Wager = Number(Phrases.shift());
-    console.log(typeof(Wager));
-
-    //error checking
-    switch (true) {
-        case !FirstWord:
-            SendHelpMessage();
-            return;
-        case isNaN(Wager):
-            RespondWith('Please enter a number to bet. "!gamble --help" for more information.');
-            return;
-    }
-
+    let HasWonTheGame=false;
+    let AmountDifference=0;
+    let AddToMessage='';
 
     switch (FirstWord) {
         case '--help':
             SendHelpMessage();
-            return;
-
+            break;
         case '--balance':
-            GetNewBalance(Gambler);
-            return;
+            UpdateBalance(Player);
+            break;
         case 'slots':
             Lines = Number(Phrases.shift());
             if (isNaN(Lines) || Lines > 5 || Lines < 1) {
-                RespondWith('Please enter a valid number of lines. Slots command is: "!gamble slots {wager} {lines 1-5}". Use "!gamble --help" for more information.');
-                return;
+                RespondWith('Invalid number of lines. Slots command is: "!gamble slots {wager} {lines 1-5}". Use "!gamble --help" for more information.');
+                break;
             }
             [HasWonTheGame, AmountDifference, AddToMessage] = SLOTS.play(Wager, Lines);
             break;
@@ -198,26 +200,22 @@ function ExecuteGame(Phrases) {
             Color = Phrases.shift();
             if (!(['red', 'black'].includes(Color))) {
                 //console.log('invalid roulette choice');
-                RespondWith('Invalid color choice. Roulette command is: "!gamble roulette {wager} {red/black}". "!gamble --help" for more information.');
+                RespondWith('Invalid color choice. Roulette command is: "!gamble roulette {wager} {red/black}". Use "!gamble --help" for more information.');
                 break;
             }
             [HasWonTheGame, AmountDifference, AddToMessage] = ROULETTE.play(Wager, Color);
             break;
         default:
-            SendHelpMessage();
-            return;
-
+            RespondWith('Sorry, that command was not recognized. Use "!gamble --help" for more information.');
+            break;
     }
-    ReplyMessage += AddToMessage;
 
-    UpdateBalance(Gambler, AmountDifference);
+    return [HasWonTheGame, AmountDifference, AddToMessage];
+};
 
-    RespondWith(GenerateResultMessage(HasWonTheGame, AmountDifference));
 
-    return;
-}
 
-//-----------------------DISCORD-CLIENT-ACTIONS-------------------------------------
+//-----------------------DISCORD_CLIENT_ACTIONS-------------------------------------
 
 // client object has permissions (intents)
 const client = new Client({
@@ -231,7 +229,7 @@ const client = new Client({
 
 client.on('ready', (cli) => {
     console.log(`${cli.user.tag} wants you to gamble.`) //startup message
-})
+});
 
 client.on('messageCreate', (message) => {
     
@@ -243,16 +241,28 @@ client.on('messageCreate', (message) => {
     let BeginningOfMessage = message.content.slice(0,7);
     if (BeginningOfMessage === '!gambl3') {
         UserMessage = message;
-        Gambler = message.author.id;
+        Player = message.author;
         ReplyMessage = '';
         MessagePhrases = ParseMessage(message);
+
     } else {
-        return
+        return;
     };
 
+
+    let HasWonTheGame, AmountDifference, AddToMessage;
     if(IsValidMessage(MessagePhrases)){
-        ExecuteGame(MessagePhrases);
+        [HasWonTheGame, AmountDifference, AddToMessage] = ExecuteGame(Player, MessagePhrases);
     };
+
+    if (AmountDifference) {
+        ReplyMessage += AddToMessage;
+
+        UpdateBalance(Player, AmountDifference);
+
+        RespondWith(GenerateResultMessage(HasWonTheGame, AmountDifference)); 
+    };
+
     return;
 });
 
@@ -271,13 +281,13 @@ function GetToken(callback) {
         DiscordBotToken = data;
         callback();
     })
-}
+};
 
 
 // callback for GetToken, gives login token to client once it is read from file
 function ClientLogin() {
     client.login(DiscordBotToken);
     return
-}
+};
 
 LoginDiscordBot = GetToken(ClientLogin);
